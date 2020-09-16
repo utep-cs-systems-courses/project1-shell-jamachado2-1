9280f1def742b9c104f58243f127d20d94b4ecd5
jamachado2 <jamachado2@miners.utep.edu>
2020-09-15 22:06:43 -0600

    updated shell

--- shell/mshell.py
+++ shell/mshell.py
@@ -6,14 +6,13 @@ import os, sys, time, re
 
 commands = ["cd", "ls", "..", ">", "exit"]
 
-
-# probably this doesn;t work because I cannot get right the 'PS1'?
 def change_dir(args):                        
-    cwd = os.getcwd()                        # return the current working directory of a process
+    cwd = os.getcwd()                    # return the current working directory of a process
     args, directory = re.split(" ", args)
-    if os.path.isdir(cwd + "/" + directory): # check if the directory exist
-        os.chdir(cwd + "/" + directory)
-
+    if os.path.isdir(cwd + "/" + args[0]): # check if the directory exist
+        os.chdir(cwd + "/" + args[0])
+        
+        
 def list_dir():                             
     cwd = os.getcwd()
     directories = os.listdir()               # return a list of the entries in the directory
@@ -23,7 +22,6 @@ def list_dir():
     
 def path(args):
     for dir in re.split(":", os.environ['PATH']): # try each directory in the path
-        args = ["wc", "mshell.py"]
         program = "%s%s" % (dir, args[0])
         try:
             os.execve(program, args, os.environ)  # try to exec program
@@ -33,28 +31,67 @@ def path(args):
         os.write(2, ("Child:    Could not exec %s\n" % args[0]).encode())
         sys.exit(1)                               # terminate with error
 
-def redirectory(args):
-    
-    user_input = input.split(args)
+def redirectoryOUT(args):
+
+    user_input = user_input.split(args)
     # Output redirection
-    if args == ">":
-        
+    if args == ">":    
         os.close(1)                 # redirect child's stdout
         os.open("mshell-output.txt", os.O_CREAT | os.O_WRONLY);
         os.set_inheritable(1, True)
         path(user_input[0].split())
-        
+
+def redirectoryIN(args):
+    user_input = user_input.split(args)
     # Input redirection
-    else: 
+    if args == "<": 
         os.close(0)                 # redirect child's stdin
         os.open("mshell-input.txt", os.O_CREAT | os.O_RDONLY);
-        os.set_inheritable(os, True)
-        path(user_input[0].split())
+        os.set_inheritable(fd, True)
+        path(args[0].split())
+        
 
-# how to pipe
-# it's kinda similar to redirectory, but with dup()
-#def pipes(args):
+def pipes(args):
+    pid = os.getpid()
+    os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
+    rc = os.fork()
+
+    if rc < 0:
+        os.write(2, ("for failed, returning %d\n" % rc).encode())
+        sys.exit(1)
+    elif rc == 0:
+        os.write(1, ("Child: My pid==%d. Parent's pid=%d\n" %(os.getpid(), pid)).encode())
+        args = user_input.split()
+
+        if "|" in user_input:
+            p = user_input.split("|")
+            pipe1 = pipe[0].split()
+            pipe2 = pipe[1].split()
     
+            # Reading and writing pipe
+            pr,pw = os.pipe()
+            for f in (pr, pw):
+                os.set_inheritable(f, true)
+
+            # Forking child
+            pf = os.fork()
+            if pf < 0:
+                print("fork failed, returning %d\n" % pf, file=sys.stderr)
+                sys.exit(1)
+            if pf == 0:
+                os.close(1)
+                os.dup(pw)                  # redirect childs stdout
+                os.set_inheritible(1, True)
+                for fd in (pr, pw):
+                    os.close(fd)
+                path(pipe1)
+            else:                           # parent fork
+                os.close(0)
+                os.dup(pr)
+                os.set_inheritable(0, True)
+                for fd in (pw, pr):
+                    os.close(fd)
+                path(pipe2)
 
 def execute(args):
     if user_input == "":
@@ -67,38 +104,26 @@ def execute(args):
     elif user_input == "ls":
         list_dir()
     elif user_input == ">":
-        redirectory(args)
+        redirectoryOUT(args)
     elif user_input == "<":
-        redirectory(args)
-    #elif args not in commands:
-    #    print(args + ": command not valid")
+        redirectoryIN(args)
+    elif user_input == "|":
+        pipes(args)
+    #elif user_input not in commands:
+    #    print(user_input + ": command not valid")
     #else:
     #    print("valid command")
 
-        
-while True:
-    if 'PS1' in os.environ:
-        os.write(1, (os.environ['PS1']).encode)
-    else:
-        os.write(1, ("$ ").encode())
-        try:
-            user_input = input()
-            execute(user_input)
-        except EOFError:
-            sys.exit(1)
-        
-pid = os.getpid()
-os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
-rc = os.fork()
 
-if rc < 0:
-    os.write(2, ("fork failed, returning %d\n" % rc).encode())
-    sys.exit(1)
-elif rc == 0:              # child
-    os.write(1, ("Child: My pid==%d. Parent's pid=%d\n" % (os.getpid(),pid)).encode())
-    args = ["wc", user_input[1]]
-
-else:                                            # parent (forked ok)
-    os.write(1, ("Parent: My pid=%d.   Child's pid=%d\n" % (pid, rc)).encode())        
-    childPidCode = os.wait()
-    os.write(1, ("Parent: child %d terminated with exit code %d\n" % childPidCode).encode())
+if __name__ == '__main__':
+    
+    while True:
+        if 'PS1' in os.environ:
+            os.write(1, (os.environ['PS1']).encode)
+        else:
+            os.write(1, ("$ ").encode())
+            try:
+                user_input = input()
+                execute(user_input)
+            except EOFError:
+                sys.exit(1)
